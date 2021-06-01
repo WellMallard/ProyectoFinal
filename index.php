0c068930faf0e0c4633f61e2bee047741159f0c0
Oscar Martinez <pumasmau13@gmail.com>
2021-06-01 11:52:40 -0500

    Se agrega el archivo index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Biblioteca de la UNAM</title>
+	</head>
+	<body>
+		<h1>Encuentra tu libro en esta tabla</h1>
+		<p>Bienvenidos a la biblioteca virtual de la UNAM</p>
+	</body>
+
+</html>
